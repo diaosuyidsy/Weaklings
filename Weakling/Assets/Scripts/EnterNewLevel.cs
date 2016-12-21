6c436381ad0dd47bb477367482b00e1ce474e55a
刁苏毅 <suyi.diao@vanderbilt.edu>
2016-12-21 13:32:25 -0600

    added multiple level

---
+++ Weakling/Assets/Scripts/EnterNewLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EnterNewLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Player"){
+			Debug.Log ("Load scene");
+			SceneManager.LoadScene (GameManager.gm.levelAfterVictory);
+		}
+	}
+}
